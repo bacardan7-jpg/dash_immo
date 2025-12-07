d84caff86864eb74d7fcc00750b539c0ae21bf96
Abdou BA <ba2664890@gmail.com>
2025-12-07 22:30:40 +0000

    V16

--- app/dashboards/__init__.py
+++ app/dashboards/__init__.py
@@ -1,5 +1,5 @@
 from .main_dashboard import EnhancedMainDashboard as MainDashboard
 from .analytics_dashboard import AnalyticsDashboard
-from .map_dashboard import MapDashboard
+from .map_dashboard import PremiumMapDashboard as MapDashboard
 
 __all__ = ['MainDashboard', 'AnalyticsDashboard', 'MapDashboard']
