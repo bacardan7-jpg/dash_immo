5977111d181ff07c5ec85acae4d7de14cf3c1856
Abdou BA <ba2664890@gmail.com>
2025-12-08 00:02:21 +0000

    V20

--- app/dashboards/main_dashboard.py
+++ app/dashboards/main_dashboard.py
@@ -12,7 +12,7 @@ from ..database.models import db, CoinAfrique, ExpatDakarProperty, LogerDakarPro
 from ..auth.decorators import analyst_required
 
 
-# Enhance
+# E
 
 class EnhancedMainDashboard:
     """Dashboard principal amélioré avec meilleures visualisations"""
